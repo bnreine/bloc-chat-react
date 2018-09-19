35f6bfff749f2f4ab464f0accf58722afe38e967
Benjamin <bnreinecke0209@gmail.com>
2018-09-19 12:10:05 -0400

    Accidental Changes on Master: List Chat Rooms

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyChxXvaAJjCj8nM-Q4q7qK8xQxnYwVLeNc",
+  authDomain: "bloc-chat-react-e1c2c.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-e1c2c.firebaseio.com",
+  projectId: "bloc-chat-react-e1c2c",
+  storageBucket: "bloc-chat-react-e1c2c.appspot.com",
+  messagingSenderId: "922282830626"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
